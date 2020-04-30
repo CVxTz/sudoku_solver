e0fa102a86310abb98a3456e7b4a8f22e38193fa
Jenazzad <mansaryouness@gmail.com>
2020-04-30 16:05:44 +0200

    refacto

--- solver/main.py
+++ solver/main.py
@@ -1,17 +1,30 @@
 import sys
 
 import numpy as np
+import cv2
+
 
 sys.path.append("..")
 from generator.Generator import Generator
 from generator import base_numbers
 from solver_models import get_model, predict
+from ocr.ocr_decoder import img_to_grid
+from ocr.ocr_detector import get_detector
+from ocr.ocr_recognizer import get_recognizer
 
 
 if __name__ == "__main__":
-    model = get_model()
+    solver = get_model()
 
-    model.load_weights("solver.h5")
+    solver.load_weights("solver.h5")
+
+    detector_model_h5 = "ocr_detector.h5"
+    detector_model = get_detector()
+    detector_model.load_weights(detector_model_h5)
+
+    recognizer_model_h5 = "ocr_recognizer.h5"
+    recognizer_model = get_recognizer()
+    recognizer_model.load_weights(recognizer_model_h5)
 
     print("Automatically Generated : ")
 
@@ -32,7 +45,7 @@ if __name__ == "__main__":
 
     x_in = [a.value for x in removed.rows.values() for a in x]
 
-    pred_gen = predict(x_in, model)
+    pred_gen = predict(x_in, solver)
 
     print("Prediction solved ?", pred_gen.board.is_solved())
 
@@ -48,9 +61,29 @@ if __name__ == "__main__":
              5, 6, 0, 0, 0, 9, 1, 0, 0,
              8, 0, 7, 0, 0, 3, 4, 0, 0]
 
-    pred_gen = predict(x_new, model)
+    pred_gen = predict(x_new, solver)
 
     print("Medium Prediction solved ?", pred_gen.board.is_solved())
     print(pred_gen.board)
+
+    print("From OCR : ")
+
+    img = cv2.imread("example1.png")
+
+    grid = img_to_grid(img, detector_model, recognizer_model, plot_path="plot.png", print_result=False)
+
+    for l in grid:
+        print(l)
+
+    x_ocr = [x for a in grid for x in a]
+
+    pred_gen = predict(x_ocr, solver)
+
+    print("OCR Prediction solved ?", pred_gen.board.is_solved())
+    print(pred_gen.board)
+
+
+
+
 
 
